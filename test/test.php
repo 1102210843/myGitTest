f3a30b6258194a2be3064f9895848f259fad6b9c
孙宇 <18610863095@163.com>
2017-09-14 00:27:52 +0800

    /

---
+++ test/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunyu
+ * Date: 2017/9/14
+ * Time: 上午12:26
+ */
+
+class test
+{
+    public function index()
+    {
+
+        echo "hello thinkPHP";
+    }
+
+
+}
