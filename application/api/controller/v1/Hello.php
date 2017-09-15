dbd577a7dc89edcf12fa3ba4d7b2ad3aa6ed5731
孙宇 <18610863095@163.com>
2017-09-15 23:28:15 +0800

    .

---
+++ application/api/controller/v1/Hello.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunyu
+ * Date: 2017/9/15
+ * Time: 下午11:23
+ */
+
+namespace app\api\controller\v1;
+
+use think\Request;
+
+class Hello
+{
+
+    public function hello()
+    {
+
+        $param = Request::instance('param');
+
+        $name = $param['name'];
+
+        return $name;
+    };
+}
