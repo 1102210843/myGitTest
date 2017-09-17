e09cb82e91725063bdfa2b462e71b0f4e288b8bf
孙宇 <18610863095@163.com>
2017-09-17 23:47:09 +0800

    a

--- application/api/model/Banner.php
+++ application/api/model/Banner.php
@@ -16,7 +16,11 @@ class Banner
 {
     public static function getBannerById($id)
     {
-        $result = Db::query('select * from banner_item where banner_id=?', [$id]);
+//        $result = Db::query('select * from banner_item where banner_id=?', [$id]);
+//        return $result;
+        $result = Db::table('banner_item')->where('banner_id', '=', $id)
+            ->select();
         return $result;
+
     }
 }
