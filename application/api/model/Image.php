c89c409cbe5e195646b9dfd2b2b0bf8c0db05336
孙宇 <18610863095@163.com>
2017-09-18 16:21:05 +0800

    a

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -2,19 +2,11 @@
 
 namespace app\api\model;
 
-use think\Model;
 
-class Image extends Model
+class Image extends BaseModel
 {
     //
     protected $hidden = ['delete_time', 'update_time', 'id', 'from'];
 
-    public function getUrlAttr($value, $data){
 
-        if ($data['from'] == 1){
-            return config('setting.img_prefix').$value;
-        }else {
-            return $value;
-        }
-    }
 }
