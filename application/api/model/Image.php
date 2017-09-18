5063a7c836e1ca7788948117996c8b8ef51dcf50
孙宇 <18610863095@163.com>
2017-09-18 16:12:08 +0800

    a

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -7,5 +7,10 @@ use think\Model;
 class Image extends Model
 {
     //
-    protected $hidden = ['delete_time', 'update_time', 'id'];
+    protected $hidden = ['delete_time', 'update_time', 'id', 'form'];
+
+    public function getUrlAttr($value){
+
+        return config('setting.img_prefix').$value;
+    }
 }
---
+++ application/extra/setting.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunyu
+ * Date: 2017/9/18
+ * Time: 下午4:04
+ */
+
+return [
+    'img_prefix' => 'http://60.205.225.243/myGitTest/public/images'
+];
